b166493ba14a72219406e329d6ab91d1e3f381e3
timmybird <bartek.svaberg@gmail.com>
2015-11-11 12:56:09 +0100

    Javadoc and comment abstract class Team

--- app/src/main/java/com/example/iths/elvira/team/Team.java
+++ app/src/main/java/com/example/iths/elvira/team/Team.java
@@ -10,25 +10,48 @@ import java.util.HashMap;
  */
 public abstract class Team implements ITeam {
 
+    // Keeps track of Team Name
     private String name;
 
+    // Keeps track of player within team
     public HashMap<Integer, Player> playerList = new HashMap<>();
 
+    /**
+     * Getter method for team name
+     * @return a String containing Team name
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter method for Team name
+     * @param name String conatining team name
+     */
     @Override
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Creates a new player by sending parameters to constructor as below
+     * @param number - player number
+     * @param firstName - player first name
+     * @param lastName - player last name
+     * this - adds team to player
+     */
     public void addPlayer(int number, String firstName, String lastName){
         new FutsalPlayer(number, firstName, lastName, this);
         playerList.put(number, new FutsalPlayer (number, firstName,lastName, this));
     }
 
+    /**
+     * Stringed method to be used if player does not exist but event needs to be added.
+     * Accepts number only. Adds empty firs name and last name.
+     * Passes parameters on to addPlayer(number, firstName lastName)
+     * @param number, int with player number
+     */
     public void addPlayer (int number) {
         addPlayer(number, "", "");
     }

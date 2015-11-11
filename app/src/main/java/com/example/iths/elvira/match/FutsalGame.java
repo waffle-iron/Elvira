06778fcca2f29359753c576f801ba30998a75873
timmybird <bartek.svaberg@gmail.com>
2015-11-11 19:42:45 +0100

    Added class Match in package match

---
+++ app/src/main/java/com/example/iths/elvira/match/FutsalGame.java
@@ -0,0 +1,13 @@
+package com.example.iths.elvira.match;
+
+import com.example.iths.elvira.team.FutsalTeam;
+
+/**
+ * Created by Bartek Svaberg on 15-11-11.
+ */
+public class FutsalGame {
+    private int lengthOPeriod, lengthOfExtraPeriod, amountOfPeriods, amountOfExtraPeriods;
+
+    FutsalTeam homeTeam = new FutsalTeam();
+    FutsalTeam awayTeam = new FutsalTeam();
+}

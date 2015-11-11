2edbc6b176a0e707838b6b79c843b78ba99ab355
nemerald <robin_wilde@hotmail.com>
2015-11-11 14:21:36 +0100

    Har lagt till TextViews till de methoder jag har gjort ändringar i. Tagit bort att värdet 0 i frisparkar och mål hämtas från activity_main.xml utan istället från variablerna jag definierat i MainActivity. Var tvungen att parsa dem för att "setText" förväntar sig en String och inte en int

--- app/src/main/java/com/example/iths/elvira/MainActivity.java
+++ app/src/main/java/com/example/iths/elvira/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.iths.elvira;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView homeTeamName, awayTeamName, homeTeamScore, awayTeamScore, homeTeamFoul, awayTeamFoul;
+
+    private String htName, atName;
+    //get default value of 0
+    private int htScore, atScore, htFoul, atFoul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeTeamName = (TextView) findViewById(R.id.tv_home_team_name);
+        awayTeamName = (TextView) findViewById(R.id.tv_away_team_name);
+        homeTeamScore = (TextView) findViewById(R.id.tv_home_team_score);
+        awayTeamScore = (TextView) findViewById(R.id.tv_away_team_score);
+        homeTeamFoul = (TextView) findViewById(R.id.tv_home_team_free_kicks);
+        awayTeamFoul = (TextView) findViewById(R.id.tv_away_team_free_kicks);
+
+        //set both teams score and foul to default (0) when created
+        homeTeamScore.setText(String.valueOf(htScore));
+        awayTeamScore.setText(String.valueOf(atScore));
+        homeTeamFoul.setText(String.valueOf(htFoul));
+        awayTeamFoul.setText(String.valueOf(atFoul));
     }
 
     public void homeTeamGoal(View view) {

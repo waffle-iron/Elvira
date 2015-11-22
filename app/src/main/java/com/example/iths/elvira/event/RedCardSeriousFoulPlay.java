a58e3b9105caadbb2aa1142087e74440f7c4f1c4
timmybird <bartek.svaberg@gmail.com>
2015-11-22 14:21:00 +0100

    Removed class RedCardSFP that's been causing all kinds of trouble. Replaced with ReCardSeriousFoulPlay

---
+++ app/src/main/java/com/example/iths/elvira/event/RedCardSeriousFoulPlay.java
@@ -0,0 +1,13 @@
+package com.example.iths.elvira.event;
+
+import com.example.iths.elvira.player.Player;
+
+/**
+ * Created by iths on 15-11-22.
+ */
+public class RedCardSeriousFoulPlay extends RedCard {
+
+    public RedCardSeriousFoulPlay(long time, Player player) {
+        super(time, player);
+    }
+}
